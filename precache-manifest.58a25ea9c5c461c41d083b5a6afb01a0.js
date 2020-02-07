0740b0341110e64505b8020dd455c32ec6aa202e
Federico Ibba <federicoibba@easy-network.it>
2020-02-07 16:22:25 +0100

    Updates

---
+++ precache-manifest.58a25ea9c5c461c41d083b5a6afb01a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539896d4885e25881a24164aec4a0e06",
+    "url": "/micro-login/index.html"
+  },
+  {
+    "revision": "2028ad51df1f90ccb811",
+    "url": "/micro-login/static/css/main.1c7cd5fc.css"
+  },
+  {
+    "revision": "2028ad51df1f90ccb811",
+    "url": "/micro-login/static/js/main.a86d5ba9.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/micro-login/static/js/main.a86d5ba9.js.LICENSE.txt"
+  }
+]);
